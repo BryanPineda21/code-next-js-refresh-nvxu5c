effa215134660dec09203eae3f48bab5b7e04ea1
BryanPineda21 <109675212+BryanPineda21@users.noreply.github.com>
2023-04-28 18:01:15 -0400

    1 file modified

--- functions.js
+++ functions.js
@@ -6,6 +6,8 @@ render('functions1', () => {
   const translate = (acronym) => {
     if (acronym === 'lol') {
       return 'laugh out loud';
+    } else if(acronym === 'idk'){
+      return "I dont know";
     } else {
       throw new Error(`idk how to translate: '${acronym}'`);
     }
@@ -14,10 +16,13 @@ render('functions1', () => {
   return translate('idk');
 });
 
+// Throws an error "idk how to"
+
+
 // TODO: Write a function that returns the bar property of an object.
 render('functions2', () => {
   const getBar = (object) => {
-
+   return object.bar
   };
 
   return getBar({ foo: 'foo', bar: 'bar' });
@@ -29,6 +34,11 @@ render('functions3', () => {
 
   const upcaseAll = (array) => {
 
+    return array.map((str)=> str.toUpperCase());
+  // for(let i=0; i < strings.length; i++){
+  //  strings[i] = strings[i].toUpperCase();
+  // }
+  // return strings;
   };
 
   return upcaseAll(strings);
